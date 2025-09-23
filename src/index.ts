202f76fb3256bdee338d7a8fc9404fee1145ef5c
SCPassion <bernard.cryptohk@gmail.com>
2025-09-23 08:59:25 +0200

    Enhance server setup by adding static file serving and health check endpoint. Updated server listening logic for clarity and maintainability.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import express from "express";
+import { handlerReadiness } from "./app/api/readiness";
 
 const app = express();
+const PORT = 8080;
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+// Serve static files from the src/app directory
+// app.use("/app" => http://localhost:8080/app/index.html
+// express.static("./src/app") => serve static files from the src/app directory
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
+
+// Start the server, listening for incoming requests on the specified port
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
+
