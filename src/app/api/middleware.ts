0df32d36dd699beb45583459a82ef4d26e12c562
SCPassion <bernard.cryptohk@gmail.com>
2025-09-24 08:37:12 +0200

    update middleware

---
+++ src/app/api/middleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Response, Request } from "express";
+
+export function middlewareLogResponses(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  res.on("finish", () => {
+    const statusCode = res.statusCode;
+    if (statusCode >= 300) {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+    }
+  });
+  next();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { handlerReadiness } from "./app/api/readiness";
+import { handlerReadiness } from "./app/api/readiness.js";
+import { middlewareLogResponses } from "./app/api/middleware.js";
 
 const app = express();
 const PORT = 8080;
@@ -7,6 +8,10 @@ const PORT = 8080;
 // Serve static files from the src/app directory
 // app.use("/app" => http://localhost:8080/app/index.html
 // express.static("./src/app") => serve static files from the src/app directory
+
+// Use the middleware on the application leve, it will subsccribe to all finish events
+app.use(middlewareLogResponses);
+
 app.use("/app", express.static("./src/app"));
 
 app.get("/healthz", handlerReadiness);
@@ -15,4 +20,3 @@ app.get("/healthz", handlerReadiness);
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
-
